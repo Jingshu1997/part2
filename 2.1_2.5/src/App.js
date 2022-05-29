b7e3e787547190c6ed5128ac70099f7fe893d888
ZHANG_Jingshu <zhangjingshu1010@gmail.com>
2022-05-29 20:46:08 +0200

    add

--- 2.1_2.5/src/App.js
+++ 2.1_2.5/src/App.js
@@ -1,56 +1,84 @@
-import Course from './component/course'
+import React from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+
+
+  const Header = (props) => {
+    return (
+      <h1>
+          Name of the course :{props.name}
+        
+      </h1>
+    )
+  }
+
+  const Content = (props) => {
+    return (
+      <div>
+        <p>
+       Part1:  {course.parts[0].name}, exercises: {course.parts[0].exercises}
+        </p>
+        <p>
+       Part2:  {course.parts[1].name}, exercises: {course.parts[1].exercises}
+        </p>
+        <p>
+       Part3: {course.parts[2].name}, exercises: {course.parts[2].exercises}
+        </p>
+      
+      
+      </div>
+    )
+  }
+  const Total = (props) => {
+
+  
+    const sum =course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises
+    return (
+      <div>
+        Number of exercises: 
+        <p>
+        {sum}
+        </p>
+
+      </div>
+    )
+  }
+
+
 
   return (
+
     <div>
-   {courses.map(course => <Course key={course.id} course={course}/>)}
+      <Header course={course} />
+      <Content  part1={course.parts[0]} part2={course.parts[1]}part3={course.parts[2]}/>
+      
+      <Total  sum={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises}/>
+ 
     </div>
+   
   )
 }
-export default App;
 
+
+
+
+export default App
