96813a7cd4c8725ff286c1bfa8017641fbbbaf65
ZHANG_Jingshu <zhangjingshu1010@gmail.com>
2022-05-30 22:14:52 +0200

    countris

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [ searchFilter,setSearchFiler ] = useState('')
+  const [ countries, setCountries] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get("https://restcountries.com/v2/all").then((res) => {
+
+        setCountries(res.data)
+      })
+  }, [])
+
+  const filteredCountries =countries.filter ( country => country.name.toLowerCase().includes(searchFilter.toLowerCase()))
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      find countries <input value={searchFilter} onChange={e => setSearchFiler(e.target.value)}/>
+      <ShowResults filteredCountries = {filteredCountries}/>
+      <showWeather filteredCountries={filteredCountries}/>
+      {filteredCountries.map(country => <div key ={country.name}>{country.name}</div>)}
+   
     </div>
-  );
+   
+  )
+
 }
 
-export default App;
+const ShowResults =({filteredCountries})=>{
+  if(filteredCountries.length === 1){
+    const country =filteredCountries[0]
+    return (
+      <div>
+        <h1>{country.name}</h1>
+       <p> capital {country.capital}</p>
+       <p> population{country.population}</p>
+       <h2>languages</h2>
+       <div>
+         <ul>{country.languages.map(language => <li>{language.name}</li>)}</ul>
+       <img src={country.flag} ></img>
+       </div>
+
+
+      </div>
+    )
+  }
+  if (filteredCountries.length > 10) return <div>Too many matches, specify another filter </div>
+  return filteredCountries.map(country => <div key ={country.name}>{country.name}<button>show</button></div>)}
+
+const showWeather=({filteredCountries})=>{
+
+return (
+  <h1>weather</h1>
+  
+)
+ 
+
+}
+
+export default App
